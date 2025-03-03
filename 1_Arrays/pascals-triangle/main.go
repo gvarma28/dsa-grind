4f514c1bb15544759f8dbf8d6563ec970c391ab5
gvarma28 <gouravswork@gmail.com>
2025-03-03 22:37:00 +0530

    feat(P-118): pascals triangle

---
+++ 1_Arrays/pascals-triangle/main.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+// https://leetcode.com/problems/pascals-triangle/
+func main() {
+	input := 5
+	res := generate(input)
+	fmt.Printf("%v", res)
+}
+func generate(numRows int) [][]int {
+	res := [][]int{}
+
+	for i := range numRows {
+		temp := []int{}
+
+		for j := 0; j <= i; j++ {
+			if j == 0 || j == len(res) {
+				temp = append(temp, 1)
+				continue
+			}
+			val := res[i-1][j-1] + res[i-1][j]
+			temp = append(temp, val)
+		}
+
+		res = append(res, temp)
+	}
+
+	return res
+}
